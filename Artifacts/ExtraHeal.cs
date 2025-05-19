6dcc53d889f44934dad9b305be382ac81a32dfaf
Havmir <theepicdm@gmail.com>
2025-05-18 19:42:27 -0500

    Add files via upload

---
+++ Artifacts/ExtraHeal.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+using Nanoray.PluginManager;
+using Nickel;
+
+namespace RuhigMod.Artifacts;
+
+// Artifact that gives all healing a 50% boost
+public class RuhigsRepairKit : Artifact, IRegisterable
+{
+    public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
+    {
+        helper.Content.Artifacts.RegisterArtifact(new ArtifactConfiguration
+        {
+            ArtifactType = MethodBase.GetCurrentMethod()!.DeclaringType!,
+            Meta = new ArtifactMeta
+            {
+                pools = [ArtifactPool.Common],
+                owner = ModEntry.Instance.RuhigDeck.Deck
+            },
+            Name = ModEntry.Instance.AnyLocalizations.Bind(["artifact", "RuhigsRepairKit", "name"]).Localize,
+            Description = ModEntry.Instance.AnyLocalizations.Bind(["artifact", "RuhigsRepairKit", "desc"]).Localize,
+ 
+            Sprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Artifact/RepairKit.png")).Sprite
+        });
+    }
+    
+    public override int ModifyHealAmount(int baseAmount, State state, bool targetPlayer)
+    {
+        return targetPlayer ? baseAmount *1/2 : 0;
+    }
+}
---
+++ Artifacts/HullAddOn.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using Nanoray.PluginManager;
+using Nickel;
+
+namespace RuhigMod.Artifacts;
+// Common Artifact that adds 5 max hull and heals 2 on pick-up ~ Havmir
+public class HullAddOn : Artifact, IRegisterable
+{
+    public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
+    {
+        helper.Content.Artifacts.RegisterArtifact(new ArtifactConfiguration
+        {
+            ArtifactType = MethodBase.GetCurrentMethod()!.DeclaringType!,
+            Meta = new ArtifactMeta
+            {
+                pools = [ArtifactPool.Common],
+                owner = ModEntry.Instance.RuhigDeck.Deck
+            },
+            Name = ModEntry.Instance.AnyLocalizations.Bind(["artifact", "HullAddOn", "name"]).Localize,
+            Description = ModEntry.Instance.AnyLocalizations.Bind(["artifact", "HullAddOn", "desc"]).Localize,
+
+            Sprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Artifact/HullAddOn.png")).Sprite
+        });
+    }
+    
+    public override string Description()
+    {
+        return "Permanently gain +5 max hull and heal 5 hull";
+    }
+    
+    public override void OnReceiveArtifact(State state)
+    {
+        state.ship.hullMax += 5;
+        int baseAmount = 5;
+        int amount = baseAmount;
+        foreach (Artifact enumerateAllArtifact in state.EnumerateAllArtifacts())
+            amount += enumerateAllArtifact.ModifyHealAmount(baseAmount, state, true);
+        state.ship.Heal(2);
+    }
+}
