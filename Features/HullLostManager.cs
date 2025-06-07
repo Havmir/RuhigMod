be34749b826c1a354078db2c21309a3a9b8825f0
Havmir <theepicdm@gmail.com>
2025-06-06 21:04:57 -0500

    Add files via upload

---
+++ Features/HullLostManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace RuhigMod.Features;
+
+// This file is used to keep track of how much hull the player has lost on a given turn, specifically for the Ruhig's Challenge Card ~ Havmir
+// Also thanks to rft50 for helping me make this :)
+
+// NOTE, you will need to make sure you add this to ModEntry, search for HullLostManager to find where ~ Havmir
+
+public class HullLostManager
+{
+
+    public void OnCombatStart(Combat combat)
+    {
+        ModEntry.Instance.Helper.ModData.SetModData(combat, "hullLostNumber", 0);
+    }
+    
+    public void OnTurnStart(Combat combat)
+    {
+        ModEntry.Instance.Helper.ModData.SetModData(combat, "hullLostNumber", 0);
+    }
+    
+    public void OnPlayerLoseHull(Combat combat, int amount)
+    {
+        if (combat.isPlayerTurn == true)
+        {
+            int hullLostNumber;
+            hullLostNumber = ModEntry.Instance.Helper.ModData.GetModDataOrDefault(combat, "hullLostNumber", 0);
+            hullLostNumber += amount;
+            ModEntry.Instance.Helper.ModData.SetModData(combat, "hullLostNumber", hullLostNumber);
+        }
+        if (combat.isPlayerTurn == false)
+        {
+            Console.WriteLine($"[Ruhig Mod] Hull Lost Manager detected: {amount} during the enemies's turn.");
+        }
+    }
+
+    public HullLostManager()
+    {
+        ModEntry.Instance.Helper.Events.RegisterBeforeArtifactsHook(nameof(HullLostManager.OnCombatStart), OnCombatStart);
+        ModEntry.Instance.Helper.Events.RegisterBeforeArtifactsHook(nameof(HullLostManager.OnTurnStart), OnTurnStart);
+        ModEntry.Instance.Helper.Events.RegisterBeforeArtifactsHook(nameof(HullLostManager.OnPlayerLoseHull), OnPlayerLoseHull);
+    }
+}
---
+++ Features/ShuffleManager.cs
@@ -0,0 +1,56 @@
+﻿namespace RuhigMod.Features;
+
+
+public class ShuffleManager
+{
+    public int _shuffleAmount;
+    public void OnCombatStart(Combat combat)
+    {
+        ModEntry.Instance.Helper.ModData.SetModData(combat, "shuffleNumber", 0);
+        ModEntry.Instance.Helper.ModData.SetModData(combat, "shuffleNumberB", 0);
+        _shuffleAmount = 0;
+    }
+    
+    public void OnTurnStart(Combat combat)
+    {
+        _shuffleAmount = 0;
+    }
+    
+    public void OnPlayerDeckShuffle(Combat combat)
+    {
+        if (_shuffleAmount > 1)
+        {
+            _shuffleAmount++;
+        }
+        if (_shuffleAmount == 1)
+        {
+            int shuffleNumberB;
+            shuffleNumberB = ModEntry.Instance.Helper.ModData.GetModDataOrDefault(combat, "shuffleNumberB", 0);
+            shuffleNumberB ++;
+            ModEntry.Instance.Helper.ModData.SetModData(combat, "shuffleNumberB", shuffleNumberB);
+            
+            _shuffleAmount++;
+        }
+        if (_shuffleAmount == 0)
+        {
+            int shuffleNumber;
+            shuffleNumber = ModEntry.Instance.Helper.ModData.GetModDataOrDefault(combat, "shuffleNumber", 0);
+            shuffleNumber ++;
+            ModEntry.Instance.Helper.ModData.SetModData(combat, "shuffleNumber", shuffleNumber);
+            
+            int shuffleNumberB;
+            shuffleNumberB = ModEntry.Instance.Helper.ModData.GetModDataOrDefault(combat, "shuffleNumberB", 0);
+            shuffleNumberB ++;
+            ModEntry.Instance.Helper.ModData.SetModData(combat, "shuffleNumberB", shuffleNumberB);
+            
+            _shuffleAmount++;
+        }
+    }
+
+    public ShuffleManager()
+    {
+        ModEntry.Instance.Helper.Events.RegisterAfterArtifactsHook(nameof(ShuffleManager.OnCombatStart), OnCombatStart);
+        ModEntry.Instance.Helper.Events.RegisterAfterArtifactsHook(nameof(ShuffleManager.OnTurnStart), OnTurnStart);
+        ModEntry.Instance.Helper.Events.RegisterAfterArtifactsHook(nameof(ShuffleManager.OnPlayerDeckShuffle), OnPlayerDeckShuffle);
+    }
+}
